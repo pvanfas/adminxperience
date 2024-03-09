67ddbc505ffc8dd501f381a5aad48b8e3f76b869
vibinvp <anfaspv.info@gmail.com>
2024-03-09 15:27:05 +0530

    up

---
+++ adminxperience/settings.py
@@ -0,0 +1,7 @@
+ADMINXPERIENCE_CONFIG = {
+    'site_title': 'AdminXperience',
+    'index_title': 'Welcome to AdminXperience',
+    'theme': 'default',
+    'logo': '/static/adminxperience/img/logo.png',
+    'favicon': '/static/adminxperience/img/favicon.png',
+}
